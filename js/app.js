c87cc5b68e9075c7902b7460398f4b025cc19634
Stiven <stivennpe@hotmail.com>
2018-08-23 22:51:27 +0200

    second commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+const firstCat = "Sallem";
+const secondCat = "Mittens";
+$(document).ready(function(){
+        $(".black").text(firstCat);
+        $(".little").html(secondCat);
+});
+
+var holi= document.getElementsByClassName('gatito');
+
+var dos=function(){
+  if(holii= true ) {
+    $(".firstclick").click(function(){
+    console.log("hola") ;
+
+    });
+  }
+
+
+};
